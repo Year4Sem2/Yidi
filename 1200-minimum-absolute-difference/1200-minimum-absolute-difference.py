2b5ce7b90528fc796794a54ed1dc095d3b1b9e74
Year4Sem2 <62819100+Year4Sem2@users.noreply.github.com>
2022-07-28 17:20:52 +0800

    Time: 660 ms (23.70%), Space: 28.8 MB (83.09%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
@@ -4,7 +4,7 @@ class Solution:
         minDiff = arr[1] - arr[0]
         for i in range(len(arr)-1):
             minDiff = min(minDiff,arr[i+1]-arr[i])
-        print(minDiff)
+        #print(minDiff)
         
         ls=[]
         for i in range(len(arr)-1):
