509bed3246a8187984c5ec9ac6eabb40481ea9b9
Year4Sem2 <62819100+Year4Sem2@users.noreply.github.com>
2022-08-04 00:08:10 +0800

    Time: 3292 ms (22.93%), Space: 18.3 MB (18.66%) - LeetHub

---
+++ 15-3sum/15-3sum.py
@@ -0,0 +1,28 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        nums.sort()
+        print(nums)
+        
+        for i in range(len(nums)):
+            if nums[i] > 0: break  # after sorting, if the min > 0, we couldn't find such three values
+            if i > 0 and nums[i] == nums[i - 1]:  # ensure that nums[i] is not duplicated
+                continue               
+            l, r = i + 1, len(nums) - 1
+            print(l, r)
+            while l < r:
+                if nums[l] + nums[r] > -nums[i]:
+                    r -= 1
+                    print(l, r)
+                elif nums[l] + nums[r] < -nums[i]:
+                    l += 1
+                    #print(l, r)
+                else:
+                    ans.append([nums[i], nums[l], nums[r]])
+					# update l to get a different sum
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+        return ans
+
+        
