8597a0c60a22a04d4b9f773e6cfc69c0d94b852a
Year4Sem2 <62819100+Year4Sem2@users.noreply.github.com>
2022-09-15 01:40:48 +0800

    Time: 621 ms (5.02%), Space: 14.2 MB (5.03%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -8,24 +8,25 @@ class Solution:
         def backtrack(cur, pos, target):
             if target == 0:
                 res.append(cur.copy())
-                #print("if target == 0, res: {}.".format(res))
+                print("if target == 0, res: {}.".format(res))
             if target <= 0:
                 return
 
             prev = -1
             for i in range(pos, len(candidates)):
-                #print("value of i: {}".format(i))
+                print("value of i: {}".format(i))
                 if candidates[i] == prev:
                     continue
                 cur.append(candidates[i])
-                #print("cur after appending: {}.".format(cur))
-                #print("backtrack({},{},{})".format(cur,i+1, target - candidates[i]))
+                print("cur after appending: {}.".format(cur))
+                print("backtrack({},{},{})".format(cur,i+1, target - candidates[i]))
                 backtrack(cur, i + 1, target - candidates[i])
-                #print("backtrack({},{},{}), getting out".format(cur[:-1],i, target))
+                print("backtrack({},{},{}), getting out, i: {}".format(cur[:-1],pos, target,i))
                 cur.pop()
-                #print("cur after popping: {}.".format(cur))
+                print("cur after popping: {}.".format(cur))
                 prev = candidates[i]
-                #print("prev: {}".format(prev))
+                print("prev: {}".format(prev))
 
         backtrack([], 0, target)
         return res
+        
